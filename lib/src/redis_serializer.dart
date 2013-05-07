f78cf8da1349a6c1083bddf9bacb43e4d1ff0d9f
Matias Meno <m@tias.me>
2013-05-07 21:46:25 +0200

    Moved the BytesEncoder to RedisSerializer.
    Closes #16

---
+++ lib/src/redis_serializer.dart
@@ -0,0 +1,40 @@
+part of redis_client;
+
+
+abstract class RedisSerializer {
+
+  factory RedisSerializer() => new JsonRedisSerializer();
+
+  List<int> serialize(Object obj);
+
+  Object deserialize(List<int> bytes);
+
+}
+
+
+
+
+class JsonRedisSerializer implements RedisSerializer {
+
+  static final int OBJECT_START = 123; // {
+  static final int ARRAY_START  = 91;  // [
+  static final int ZERO         = 48;  // 0
+  static final int NINE         = 57;  // 9
+  static final int SIGN         = 45;  // -
+
+  static final String DATE_PREFIX = "/Date(";
+  static final String DATE_SUFFIX = ")/";
+  static final String TRUE  = "true";
+  static final String FALSE = "false";
+
+  List<int> serialize(Object obj) => stringify(obj).runes.toList(growable: false);
+
+
+  Object deserialize(List<int> bytes) {
+    if (bytes == null || bytes.length == 0) return null;
+
+    return JSON.parse(new String.fromCharCodes(bytes));
+  }
+
+
+}
