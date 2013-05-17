46c28132c0ad7a348611d018c16fbe1c64e0c637
Matias Meno <m@tias.me>
2013-05-17 12:09:29 +0200

    Added library documentation.

--- lib/redis_protocol_transformer.dart
+++ lib/redis_protocol_transformer.dart
@@ -1,3 +1,21 @@
+
+/**
+ * This is a [StreamTransformer] for redis Socket replies.
+ *
+ * It converts a binary stream from a redis socket to [RedisReply] objects.
+ *
+ * You can pass it as a transformer to a Socket like this:
+ *
+ *     socket.transform(new RedisProtocolTransformer()).listen((RedisReply reply) { });
+ *
+ * There are five types of [RedisReply]s:
+ *
+ *  - [ErrorReply]
+ *  - [StatusReply]
+ *  - [IntegerReply]
+ *  - [BulkReply]
+ *  - [MultiBulkReply]
+ */
 library redis_protocol_transformer;
 
 import "dart:async";
@@ -11,6 +29,4 @@ part 'transformer/transformer.dart';
 part 'transformer/data_consumers.dart';
 
 part 'transformer/redis_replies.dart';
-
-
 
