765649593b36e2badf4b1f0f7313be41593bbd65
Tomasz Kubacki <tomasz.kubacki@gmail.com>
2014-02-03 14:12:20 +0100

    added pubsub tests to all tests

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -3,6 +3,7 @@ import "redis_serializer_tests.dart" as redisSerializerTests;
 import "redis_client_tests.dart" as redisClientTests;
 import "redis_connection_tests.dart" as redisConnectionTests;
 import "redis_protocol_transformer_tests.dart" as redisProtocolTransformerTests;
+import "redis_pubsub_tests.dart" as redisPubSubTests;
 
 
 main() {
@@ -16,10 +17,15 @@ main() {
 
   redisSerializerTests.main();
 
+  ///this seems to not work on Linux/64 Redis 2.6.13 
+  ///error msg: https://gist.github.com/tomaszkubacki/8783531
   redisClientTests.main();
 
   redisConnectionTests.main();
 
   redisProtocolTransformerTests.main();
+  
+  redisPubSubTests.main();
+  
 
 }
